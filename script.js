393d3fda1ce229401d12e0b5f7594da445158f88
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-07 23:22:28 +0200

    created base operator functions

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+
+let addFunction = (a, b) => a + b;
+let subFunction = (a, b) => a - b;
+let multFunction = (a, b) => a * b;
+let divFunction = (a, b) => a / b;
+
+//testing function
+console.log(addFunction(5, 3));
