b325f7a0a3cbfc9691fd93911586a831b061b929
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-07 23:25:53 +0200

    created operate function

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ let subFunction = (a, b) => a - b;
 let multFunction = (a, b) => a * b;
 let divFunction = (a, b) => a / b;
 
-//testing function
-console.log(addFunction(5, 3));
+let operate = (operator, a, b) => operator(a, b);
+
+console.log(operate(addFunction, 2, 9));
