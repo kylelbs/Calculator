0424a834f6abae2eda26b8d14c06a6e89ddc4eb3
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-09 00:55:58 +0200

    pushed for correction/help

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 console.log("Hello world");
 
-let addFunction = (a, b) => a + b;
-let subFunction = (a, b) => a - b;
-let multFunction = (a, b) => a * b;
-let divFunction = (a, b) => a / b;
+const addFunction = (a, b) => a + b;
+const subFunction = (a, b) => a - b;
+const multFunction = (a, b) => a * b;
+const divFunction = (a, b) => a / b;
+
+const opToFunction = {
+    "+": "addFunction",
+    "-": "subFunction",
+    "*": "multFunction",
+    "/": "divFunction",
+};
 
 let operate = (operator, a, b) => operator(a, b);
 
-buttons = document.querySelectorAll("button");
-display = document.querySelector('#display');
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector('#display');
+const equal = document.querySelector('#equal');
+let calcLine = '';
+
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
         display.innerText += e.target.innerText;
+        calcLine += e.target.innerText;
+        console.log(calcLine);
     });
 });
+
+equal.addEventListener('click', () => {
+    let calcArray = calcLine.split("");
+    calcArray.pop();
+    // console.log(operate(addFunction, parseInt(calcArray[0]), parseInt(calcArray[2])))
+    let result = operate(Function(opToFunction[`${calcArray[1]}`]), parseInt(calcArray[0]), parseInt(calcArray[2]));
+    console.log(result);
+    display.innerText = Number(result);
+
+});
+
