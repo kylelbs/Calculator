cf6883bdea63bd8df910aaea039d0e44b0d93d2f
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-10 15:26:13 +0200

    Bug correction (impossible operations)

--- script.js
+++ script.js
@@ -17,44 +17,73 @@ let operate = (operator, a, b) => operator(a, b);
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector('#display');
 const equal = document.querySelector('#equal');
+const operators = document.querySelectorAll('.operator');
 let calcLine = '';
 
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
-        if (e.target.id != "equal") {
-            display.innerText += e.target.innerText;
-            calcLine += e.target.innerText;
-            console.log(calcLine);
+        for (let key in opToFunction) {
+            // console.log(key);
+            // console.log(calcLine.slice(-1));
+            if (
+                calcLine.slice(-1) == key &&
+                e.target.className == "operator") {
+                //truc a mettre l)
+                console.log(calcLine);
+                console.log("Invalid");
+                calcLine.slice(-1);
+                console.log(calcLine)
+                break;
+            }
+            if (e.target.id != "equal") {
+                display.innerText += e.target.innerText;
+                calcLine += e.target.innerText;
+                console.log(calcLine);
+                break;
+            }
         }
     });
 });
 
 equal.addEventListener('click', () => {
 
-    let numbers = calcLine.split(/[*]|[+]|[-]|[\/]/);
-    let operators = calcLine.split(/[^-|+|\/|\*]/).filter(e => e);
+    let isCalcPossible = true;
 
-    // console.log(numbers);
-    // console.log(operators);
-
-    let calcArray = numbers.map(
-        (element, index) => [element, operators[index]]
-    ).flat();
-    calcArray.pop();
-
-    // console.log(calcArray);
-
-    let result = parseInt(operate(opToFunction[`${calcArray[1]}`], parseInt(calcArray[0]), parseInt(calcArray[2])));
-    if (calcArray.length > 3) {
-        for (let i = 3; i < calcArray.length; i += 2) {
-            result = operate(opToFunction[`${calcArray[i]}`], result, parseInt(calcArray[i + 1]));
+    for (let key in opToFunction) {
+        if (calcLine.slice(-1) == key) {
+            console.log("You can't do that");
+            isCalcPossible = false;
+            break;
         }
     };
 
-    console.log(result);
-    display.innerText = result.toString();
-    calcLine = result;
+    if (isCalcPossible == true) {
+        let numbers = calcLine.split(/[*]|[+]|[-]|[\/]/);
+        let operators = calcLine.split(/[^-|+|\/|\*]/).filter(e => e);
+
+        // console.log(numbers);
+        // console.log(operators);
+
+        let calcArray = numbers.map(
+            (element, index) => [element, operators[index]]
+        ).flat();
+        calcArray.pop();
+
+        // console.log(calcArray);
+
+        let result = parseInt(operate(opToFunction[`${calcArray[1]}`], parseInt(calcArray[0]), parseInt(calcArray[2])));
+        if (calcArray.length > 3) {
+            for (let i = 3; i < calcArray.length; i += 2) {
+                result = operate(opToFunction[`${calcArray[i]}`], result, parseInt(calcArray[i + 1]));
+            }
+        };
+
+        console.log(result);
+        display.innerText = result.toString();
+        calcLine = result.toString();
+
+    };
 
 });
 
